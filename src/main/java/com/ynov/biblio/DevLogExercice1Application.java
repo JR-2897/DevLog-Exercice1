ef2cd5c8b4448bceb9141ce43c91941173151e2a
JR-2897 <jean.ragueneau.pro@gmail.com>
2020-11-06 10:28:38 +0100

    Test commit

---
+++ src/main/java/com/ynov/biblio/DevLogExercice1Application.java
@@ -0,0 +1,13 @@
+package com.ynov.biblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLogExercice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevLogExercice1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynov/biblio/DevLogExercice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.biblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevLogExercice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
